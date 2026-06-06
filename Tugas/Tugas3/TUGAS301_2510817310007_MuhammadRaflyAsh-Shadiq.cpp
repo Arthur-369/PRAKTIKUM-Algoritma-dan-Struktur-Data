d3817df2d07276474b13cd683922461612d040dc
Arthur-369 <rafly8014@gmail.com>
2026-06-06 19:33:50 +0700

    Add files via upload

--- Tugas/Tugas3/TUGAS301_2510817310007_MuhammadRaflyAsh-Shadiq.cpp
+++ Tugas/Tugas3/TUGAS301_2510817310007_MuhammadRaflyAsh-Shadiq.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void generateRandom(int arr[], int n);
 void insertionSort(int arr[], int n);
-bool binarySearch(int arr[], int kiri, int kanan, int target);
+int binarySearchCount(int arr[], int kiri, int kanan, int target);
 
 int main() {
     srand(time(0));
@@ -16,7 +16,7 @@ int main() {
     cin >> n >> x;
 
     if(n < 10) {
-        cout << "Jumlah data minimal 10" << endl;
+        cout << "Jumlah data minimal 10!" << endl;
         return 0;
     }
 
@@ -29,25 +29,22 @@ int main() {
     }
 
     insertionSort(arr, n);
-
     cout << "\n\nData Setelah Diurutkan :" << endl;
     for(int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
 
     int jumlahPasangan = 0;
-
     for(int i = 0; i < n - 1; i++) {
-
-        if(i > 0 && arr[i] == arr[i - 1]) {
-            continue;
-        }
 
         int target = x - arr[i];
 
-        if(binarySearch(arr, i + 1, n - 1, target)) {
-            jumlahPasangan++;
-        }
+        jumlahPasangan += binarySearchCount(
+            arr,
+            i + 1,
+            n - 1,
+            target
+        );
     }
 
     cout << "\n\nJumlah pasangan = "
@@ -64,9 +61,9 @@ void generateRandom(int arr[], int n) {
 
 void insertionSort(int arr[], int n) {
     for(int i = 1; i < n; i++) {
+
         int key = arr[i];
         int j = i - 1;
-
         while(j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
@@ -75,12 +72,14 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-bool binarySearch(int arr[], int kiri, int kanan, int target) {
+int binarySearchCount(int arr[], int kiri, int kanan, int target) {
+    int posisi = -1;
     while(kiri <= kanan) {
         int tengah = (kiri + kanan) / 2;
 
         if(arr[tengah] == target) {
-            return true;
+            posisi = tengah;
+            break;
         }
         else if(target < arr[tengah]) {
             kanan = tengah - 1;
@@ -89,5 +88,20 @@ bool binarySearch(int arr[], int kiri, int kanan, int target) {
             kiri = tengah + 1;
         }
     }
-    return false;
+    if(posisi == -1) {
+        return 0;
+    }
+
+    int jumlah = 1;
+    int i = posisi - 1;
+    while(i >= kiri && arr[i] == target) {
+        jumlah++;
+        i--;
+    }
+    i = posisi + 1;
+    while(i <= kanan && arr[i] == target) {
+        jumlah++;
+        i++;
+    }
+    return jumlah;
 }
